3f086a73fdea7cf0540e9bffaed20ac4b70ecce6
Kane Swartz <kane.swartz@myport.ac.uk>
2021-06-10 18:31:42 +0100

    front end updates

--- client/script.js
+++ client/script.js
@@ -1,10 +1,11 @@
-const ws = new WebSocket('ws://' + window.location.hostname + ':' +
-  (window.location.port || 80) + '/');
+let ws = new WebSocket('ws://' + window.location.hostname + ':' + (window.location.port || '80'));
 const textToTranslate = document.querySelector('#text-to-translate');
 const translatedText = document.querySelector('#translated-text');
 const languageSelector = document.querySelector('#language-selector');
+const translateServerAddress = document.querySelector('#translate-server-address');
+const serverAddressUpdate = document.querySelector('#server-address-update');
 
-function toServer(e) {
+function toServer() {
   ws.send(JSON.stringify({
     text: textToTranslate.value,
     target: languageSelector.value
@@ -13,9 +14,19 @@ function toServer(e) {
 
 function fromServer(e) {
   translatedText.textContent = e.data;
-  console.log(e);
 }
 
-textToTranslate.addEventListener('keyup', toServer);
-languageSelector.addEventListener('change', toServer);
-ws.addEventListener('message', fromServer);
+function updateWebSocket() {
+  ws = new WebSocket('ws://' + translateServerAddress.value);
+  ws.addEventListener('message', fromServer);
+}
+
+function init() {
+  translateServerAddress.value = window.location.hostname + ':' + (window.location.port || '80');
+  textToTranslate.addEventListener('keyup', toServer);
+  languageSelector.addEventListener('change', toServer);
+  serverAddressUpdate.addEventListener('click', updateWebSocket);
+  updateWebSocket();
+}
+
+window.addEventListener('load', init)
