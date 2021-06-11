9b79ca0c75bfa3bfa354b8ac869b08bf9bb5bc21
Kane Swartz <kane.swartz@myport.ac.uk>
2021-06-11 12:00:40 +0100

    text sent at intervals

--- client/script.js
+++ client/script.js
@@ -36,7 +36,7 @@ function runSimulation() {
     'a palm tree',
   ];
   for (let i = 0; i < numberOfClients; i += 1) {
-    const newClient = new client(thingsToTranslate, translatorWebsocket, targetLanguage);
+    const newClient = new client([...thingsToTranslate], translatorWebsocket, targetLanguage);
     newClient.init();
     clients.push(newClient);
   }
@@ -69,35 +69,44 @@ function init() {
 window.addEventListener('load', init)
 
 class client {
-  constructor(textArray, translatorWebsocket, targetLanguage) {
+  constructor(textArray, translatorWebsocket, targetLanguage, textRate) {
     this.textArray = textArray ; // lines of text to send to translator
     this.translatorWebsocket = translatorWebsocket;
     this.targetLanguage = targetLanguage;
     this.translations = [];
+    this.intervalId = null;
+    this.intervalTime = textRate;
+    this.boundRequestTranslation = null; // required for setInterval https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval#the_this_problem
   }
 
   init() {
     this.ws = new WebSocket(this.translatorWebsocket);
     this.ws.addEventListener('open', () => { this.openWebsocket() });
     this.ws.addEventListener('message', (event) => { this.receiveTranslation(event) });
+    this.boundRequestTranslation = this.requestTranslation.bind(this);
   }
 
   openWebsocket() {
-    for (const text of this.textArray) {
-      this.requestTranslation(text);
-    }
+    this.intervalId = setInterval(this.boundRequestTranslation, this.intervalTime);
   }
 
-  requestTranslation(text) {
-    this.ws.send(JSON.stringify({
-      text: text,
-      target: this.targetLanguage,
-    }));
+  requestTranslation() {
+    if (this.textArray.length > 0) {
+      this.ws.send(JSON.stringify({
+        text: this.textArray.pop(),
+        target: this.targetLanguage,
+      }));
+    } else {
+      window.clearInterval(this.intervalId);
+      this.ws.close();
+    }
   }
 
   receiveTranslation(event) {
     this.translations.push(event.data);
-    console.log(event.data);
+    console.log(`speakerId: ${this.intervalId}, speakerRate${this.intervalTime}`);
+    console.log(`translation: ${event.data}`);
+    console.log('//////');
   }
 }
 
