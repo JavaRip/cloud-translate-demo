24e120a98c0bf7350d1e7976758f00e44ef6a738
kane <UP941226@myport.ac.uk>
2021-08-10 14:16:19 +0100

    change html to hyphen case

--- client/classes/elements.js
+++ client/classes/elements.js
@@ -1,5 +1,6 @@
 export class Elements {
   constructor() {
+    this.wrapper = document.querySelector('#wrapper');
     this.textToTranslate = document.querySelector('#text-to-translate');
     this.translatedText = document.querySelector('#translated-text');
     this.languageSelector = document.querySelector('#language-selector');
@@ -12,8 +13,8 @@ export class Elements {
     this.serverAddressUpdate = document.querySelector('#server-address-update');
     this.logs = document.querySelector('#logs');
     this.nav = document.querySelector('nav');
-    this.simulationLog = document.querySelector('#simulationLog');
-    this.clientLog = document.querySelector('#clientLog');
-    this.translationLog = document.querySelector('#translationLog');
+    this.simulationLog = document.querySelector('#simulation-log');
+    this.clientLog = document.querySelector('#client-log');
+    this.translationLog = document.querySelector('#translation-log');
   }
 }
